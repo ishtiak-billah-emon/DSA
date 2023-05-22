798da99c57e5653261ae1688fca6d86b8a21ca18
Ishtiak Billah Emon <ishtiakemon2002@gmail.com>
2023-05-22 12:02:20 +0600

    reverse an array

--- L09-Array/Array_reverse.cpp
+++ L09-Array/Array_reverse.cpp
@@ -5,7 +5,8 @@ int main()
     int n = 7;
     int a[n]={1,2,3,4,5,6,7};
 
-
+    // way 1
+    
     for(int i = 0; i< n/2; i++)
     {
             swap(a[i],a[n-i-1]);
@@ -38,6 +39,19 @@ int main()
         swap( 2 , 5)
         swap( 3 , 4)
     */
+
+   // way 2
+
+   int start = 0;
+   int end = n-1 ; 
+
+   while( start <= end)
+   {
+        swap(a[start],a[end]);
+        start ++;
+        end --;
+   }
+   
    // printing array elements
     for(int i = 0; i<n;i++)
     {
