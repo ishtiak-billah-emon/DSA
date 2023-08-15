0762ee1e35f83c7eae722f2495f04c1fb42e364a
Ishtiak Billah Emon <ishtiakemon2002@gmail.com>
2023-08-15 13:06:49 +0600

    done

---
+++ Folderr/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int add(int a, int b){
+	return (a+b);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a , b; cin>>a>>b;
+    cout<<add(a,b)<<'\n';
+
+
+    return 0;
+}
